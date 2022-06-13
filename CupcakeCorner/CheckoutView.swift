7b9e15344fa0600ab6623f0f84cff09ed8d5415a
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-13 01:21:43 -0400

    add accessibility

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -21,13 +21,14 @@ struct CheckoutView: View {
         ScrollView {
             VStack {
                 AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
-                        image
-                            .resizable()
-                            .scaledToFit()
+                    image
+                        .resizable()
+                        .scaledToFit()
                 } placeholder: {
                     ProgressView()
                 }
                 .frame(height: 233)
+                .accessibilityHidden(true)
 
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
